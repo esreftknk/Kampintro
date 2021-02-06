d69cafb84899e08fb2b38f2d6880db113bcbcd63
EŞREF TEKİNOK <esref_lionking_41@hotmail.com>
2021-02-06 11:53:08 +0300

    5.gün ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İş Sınıfları // Buniness Classes
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+
+        internal void Ekle(object musteri2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

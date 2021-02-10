cc6e91cfb5bf593692dc234afbd437c9e0be9c15
EŞREF TEKİNOK <esref_lionking_41@hotmail.com>
2021-02-10 23:11:52 +0300

    Ödev

---
+++ MyListOdev/Datalist.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyListOdev
+{
+
+    //Array tutma ve yönetme
+    
+    class Datalist <T>
+    {
+        T[] items;
+        //Constructor
+        public Datalist()
+        {
+            items = new T[0];
+
+        }
+
+        //T key değerini dinamik artırma kodu
+        public void  Add (T item)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+
+    }
+}
---
+++ MyListOdev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyListOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
